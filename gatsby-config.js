ca719bc0bd556116593630f9b2d8b72d414434a1
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-02-04 17:38:49 +0100

    latest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Dragos Nedelcu`,
+    author: `Dragos Nedelcu`,
+    description: `A starter blog powered by Gatsby.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `kylemathews`,
