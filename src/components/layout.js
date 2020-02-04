85e979c40053a775518e76180506c8b298c6353d
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-02-04 23:19:25 +0100

    getting used to the congig

--- src/components/layout.js
+++ src/components/layout.js
@@ -54,7 +54,7 @@ const Layout = ({ location, title, children }) => {
       style={{
         marginLeft: `auto`,
         marginRight: `auto`,
-        maxWidth: rhythm(24),
+        maxWidth: rhythm(28),
         padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
       }}
     >
