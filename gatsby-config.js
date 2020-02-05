36dbe323a47c7e8faa163f2b8c11a78d903df991
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-02-05 09:56:16 +0100

    google analyttics

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `AUA-62374624-1`,
       },
     },
     `gatsby-plugin-feed`,
