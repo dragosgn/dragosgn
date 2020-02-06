d163b0ea83ce7a3220e1045018418deeb950000a
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-02-06 16:14:17 +0100

    better fonts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -13,6 +13,7 @@ Wordpress2016.overrideThemeStyles = () => ({
   hr: {
     background: "var(--hr)",
   },
+
   "a.gatsby-resp-image-link": {
     boxShadow: "none",
   },
@@ -45,7 +46,11 @@ Wordpress2016.overrideThemeStyles = () => ({
 
 delete Wordpress2016.googleFonts
 
-const typography = new Typography(Wordpress2016)
+const typography = new Typography({
+  ...Wordpress2016,
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+})
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== "production") {
