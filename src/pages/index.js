64da8abc8d78eb6140251f363a691c59605a3e00
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-02-06 14:35:04 +0100

    fix home

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,29 +30,34 @@ const BlogIndex = ({ data, location }) => {
           borderRadius: `50%`,
         }}
       />
+      <p>Hi, I am Dragos.</p>
+
       <p>
-        <br />
-        Hi, I am Dragos. <br /> <br />A Software Engineer currently at Mister
-        Spex, supporting a fantastic team on building the best place to buy
-        eyewear in Europe. On the side, I share my knowledge and passion for
-        software with 10+ amazing students at ESMT Berlin. <br />
-        <br />
+        A Software Engineer currently at Mister Spex, supporting a fantastic
+        team on building the best place to buy eyewear in Europe. On the side, I
+        share my knowledge and passion for software with 10+ amazing students at
+        ESMT Berlin.
+      </p>
+      <p>
         Born in Romania, raised in Spain now based in beautiful Berlin. This
         diversity of experiences, people, and perspectives marked my career. I
         hold a Bachelor in Aerospace Engineering with a minor in Propulsion
-        Systems. <br />
-        <br />
+        Systems.
+      </p>
+      <p>
         Building software products, teams, and companies has been my fascination
         from a young age. On the way, I had the opportunity to lead different
         software startups, sometimes a Co-founder. Always with a strong focus on
-        delivery and results. <br />
-        <br />
+        delivery and results.
+      </p>
+      <p>
         It's been quite a ride, and I love every minute of it. As for the
         reader, work with people you admire, focus on contribution, and never
-        settle. <br />
+        settle.
+      </p>
+      <p>
+        Until the next one :)
         <br />
-        <br />
-        Until the next one :) <br />
         Dragos
       </p>
     </Layout>
