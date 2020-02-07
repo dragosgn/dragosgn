8cab566e094b3f295d17653465747b1ae83a8694
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-02-07 16:45:03 +0100

    adding des

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -70,6 +70,7 @@ export const pageQuery = graphql`
           frontmatter {
             date(formatString: "MMMM DD, YYYY")
             title
+            description
           }
         }
       }
