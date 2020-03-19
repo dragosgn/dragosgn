7b0fab772f9e27bbdce627a005fa8051def4c96b
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-03-19 21:21:47 +0100

    fixes

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,30 +4,10 @@ import { useStaticQuery, graphql, Link } from "gatsby"
 import { rhythm } from "../utils/typography"
 
 const Footer = () => {
-  const {
-    site: {
-      siteMetadata: { author },
-    },
-  } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            author
-            social {
-              twitter
-            }
-          }
-        }
-      }
-    `
-  )
-
   return (
     <footer style={{ paddingTop: `${rhythm(1 / 4)}` }}>
-      © {new Date().getFullYear()}, built with ❤️ by{" "}
-      <Link to={`/`}>{author}</Link> for humans.
+      © {new Date().getFullYear()}, built with ❤️ by <Link to={`/`}>me</Link>{" "}
+      for humans.
     </footer>
   )
 }
