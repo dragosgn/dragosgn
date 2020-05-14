12eaaa1c95723d941589a6dfcc9f3690f8ef3137
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-05-14 23:54:34 +0200

    fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,30 +36,48 @@ const BlogIndex = ({ data, location }) => {
       />
       <h3>Hi, I am Dragos.</h3>
       <p style={{ textAlign: "justify" }}>
-        A Software Engineer currently at Mister Spex, supporting a fantastic
-        team on building the best place to buy eyewear in Europe. On the side, I
-        share my knowledge and passion for software with 10+ amazing students at
-        ESMT Berlin.
+        I am a Software Engineer supporting a fantastic team at Mister Spex. On
+        the side, I share my knowledge and passion for software by mentoring 15+
+        incredible software developers on their journey.
       </p>
       <p style={{ textAlign: "justify" }}>
-        Born in Romania, raised in Spain now based in beautiful Berlin. This
+        Born in Romania, raised in Spain and now based in beautiful Berlin. This
         diversity of experiences, people, and perspectives marked my career. I
         hold a Bachelor in Aerospace Engineering with a minor in Propulsion
         Systems.
       </p>
       <p style={{ textAlign: "justify" }}>
-        Building software products, teams, and companies has been my fascination
-        from a young age. On the way, I had the opportunity to lead different
-        software startups, sometimes as a Co-founder. Always with a strong focus
-        on delivery and results.
+        Bankrupt before 23, when my software consulting company went burst, I
+        went on and built a career in the industry. It meant moving(again) to a
+        new country, sleeping on my friend's couch, and burning the midnight
+        oil.
+      </p>
+      <p style={{ textAlign: "justify" }}>
+        Since then, I've made my way up. After exploring Berlin's startup
+        ecosystem, I ended up joining one of Europe's top tech companies -
+        Mister Spex. Passionate about leadership, I perform best at the
+        intersection of the intersection between people, technology, and
+        business.
       </p>
       <p style={{ textAlign: "justify" }}>
         It's been quite a ride, and I love every minute of it. As for the
-        reader, work with people you admire, focus on contribution, and never
-        settle.
+        reader, work with people you admire and never settle.
       </p>
+      <br></br>
+      <p style={{ textAlign: "justify" }}>
+        ⇒ For mentoring, book me{" "}
+        <a href=" https://www.mentoring-club.com/the-mentors/dragos-nedelcu">
+          here.
+        </a>
+      </p>
+      <p style={{ textAlign: "justify" }}>
+        ⇒ I am always looking for projects with an impact. Looking for a tech
+        co-founder? Drop me a line: dragosgn@gmail.com{" "}
+        <span role="img">🚀</span>
+      </p>
+
       <p>
-        You can see more of me here:{" "}
+        ⇒ More of me on:{" "}
         <a href={linkedin} target="_blank" rel="noopener noreferrer">
           Linkedin
         </a>{" "}
