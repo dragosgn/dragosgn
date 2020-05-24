8bbaa4ffc86b250f52272d1487a64fd3cca3a05f
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-05-24 18:03:53 +0200

    lastest

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,14 +6,14 @@ import { rhythm } from "../utils/typography"
 export default () => (
   <footer style={{ paddingTop: `${rhythm(1 / 4)}` }}>
     © {new Date().getFullYear()}, built with ❤️ in Berlin by{" "}
-    <Link to={`/`}>me</Link> for humans using{" "}
+    <Link to={`/`}>me</Link> for humans. This website uses{" "}
     <a
       href="https://www.gatsbyjs.org/"
       target="_blank"
       rel="noopener noreferrer"
     >
       Gatsby.js
-    </a>
-    .
+    </a>{" "}
+    with some personal tweaks .
   </footer>
 )
