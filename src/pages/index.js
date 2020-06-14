00e2b5a05bcf72a30757245d57aa756104d89773
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-06-14 11:04:41 +0200

    updated bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,52 +33,48 @@ const BlogIndex = ({ data, location }) => {
       />
       <h3>Hi, I am Dragos.</h3>
       <p style={{ textAlign: "justify" }}>
-        I am a self-taught Software Developer at Mister Spex. As a mentor, I
-        help professionals in their 30ies and 40ies to get a job as a developer
-        in 3-6 months, without a degree or Bootcamp. The catch? I've done it
-        before. Bankrupt before 23, when my consulting company went burst, I
-        went on and built a career in the industry and ended up joining one of
-        Europe's top tech companies.
+        Self-taught Software Developer and Mentor. <br />
+        <br /> As a mentor, I coach professionals in their 30ies and 40ies to
+        get a job as a developer in 3-6 months, without a degree or a Bootcamp.
+        The catch? I've done it before. After my consulting company went
+        bankrupt, I've built a career in the software industry and ended up
+        joining one of Europe's top tech companies.
       </p>
       <p style={{ textAlign: "justify" }}>
-        Last year, I've mentored more than 20 developers on their journey, many
-        of them getting full-time jobs. I specialized in 'outsiders' wanting to
-        get a job as a developer. I help them gain skills relevant to the
-        current market using a clear leaning plan, live coding sessions, and
-        industry expertise. I then help them get interviews and pass technical
-        challenges and finally get hired.
+        I specialize in 'outsiders' wanting to get a job as a developer. So far,
+        I've mentored 20+ developers on their journey, many of them getting
+        full-time jobs. I help them gain skills relevant to the current market
+        using a clear leaning plan, live coding sessions, and industry
+        expertise. I then help them get interviews, pass technical challenges
+        and finally get hired.
       </p>
       <p style={{ textAlign: "justify" }}>
-        I use a 'hands-on' approach focusing on both practical and theoretical
-        aspects of software development. With the right mindset, a clear
-        learning plan, dedication, and guidance from someone that has "been
-        there and done that", anyone can get a job in tech.
+        My 'hands-on' approach focuses on both practical and theoretical aspects
+        of software development. With the right mindset, a clear learning plan,
+        dedication, and guidance from someone that has "been there and done
+        that", anyone can get a job in tech.
       </p>
+      <br />
       <p style={{ textAlign: "justify" }}>
-        Since then, I've made my way up. After exploring Berlin's startup
-        ecosystem, I ended up joining one of Europe's top tech companies -
-        Mister Spex. Passionate about leadership, I perform best at the
-        intersection between technology, people and business.
-      </p>
-      <p style={{ textAlign: "justify" }}>
-        It's been quite a ride, and I love every minute of it. As for the
-        reader, work with people you admire and never settle.
-      </p>
-      <br></br>
-      {/* <p style={{ textAlign: "justify" }}>
-        ⇒ For mentoring, book me{" "}
-        <a href=" https://www.mentoring-club.com/the-mentors/dragos-nedelcu">
+        👉🏻 You can book a quick call with me
+        <a
+          href="https://calendly.com/dragosgn/book-dragos"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
           here.
         </a>
-      </p> */}
-      <p style={{ textAlign: "justify" }}>
-        ⇒ I am always looking for projects with an impact. Looking for a tech
-        co-founder? Drop me a line: dragosgn@gmail.com{" "}
-        <span role="img">🚀</span>
+        <span role="img"> 🚀</span>
       </p>
 
+      {/* <p style={{ textAlign: "justify" }}>
+        👉🏻 I am always looking for projects with an impact. Looking for a tech
+        co-founder? Drop me a line: dragosgn@gmail.com.
+      </p> */}
+      <br />
       <p>
-        ⇒ More of me on:{" "}
+        More of me on:{" "}
         <a href={linkedin} target="_blank" rel="noopener noreferrer">
           Linkedin
         </a>{" "}
